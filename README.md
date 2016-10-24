0d84a77375a29f54adeb55815a398e87d1670e64
Ryan Lissack <rlissack@MacBook.home>
2016-10-24 10:25:18 -0700

    Boilerplate React App Running with webpack, react-dom and babel

---
+++ README.md
@@ -0,0 +1,5 @@
+https://edspencer.net/2016/03/20/a-new-stack-for-2016-getting-started-with-react-es6-and-webpack/#comments
+
+Don't forget to install webpack globally and have it running in the root of this project while making changes:
+
+  $ sudo npm install -g webpack
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Paragraph from './Paragraph';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="my-app">
+        <h1>This is React!!!</h1>
+        <Paragraph text="First Paragraph" />
+        <Paragraph text="Second Paragraph" />
+      </div>
+    );
+  }
+}
